55de2528eb21699016b406405b831c56c4990357
thicca <thicca.land@gmail.com>
2026-08-10 21:16:38 +0900

    contact.phpに入れ替え忘れてたheadとheaderの呼び出し処理を対応

--- public/contact.php
+++ public/contact.php
@@ -7,57 +7,10 @@ $cssFile = 'contact';
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-  <head>  
-    <meta charset="utf-8">
-    <title>Contact | Thicca's HP</title>
-    <meta name="description" content="Thicca's HPのお問い合わせページです。ご不明点やご質問などございましたらお気軽にお問い合わせください。">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="images/common/favicon.ico">
-    <link rel="apple-touch-icon" href="images/common/apple-touch-icon.png">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;500;700&family=Zen+Old+Mincho:wght@400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/contact.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="js/main.js"></script>
-  </head>
+  <?php require_once __DIR__ . '/../includes/head.php'; ?>
 
   <body>
-    <header id="header">
-      <div class="logo">
-        <a href="index.php">
-          <img src="images/common/logo.svg" alt="Thicca's HP">
-        </a>
-      </div>
-
-      <div class="hamburger">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-
-      <nav class="navi">
-        <ul class="menu">
-          <li class="menu-first">
-            <span>会社情報</span>
-            <ul class="menu-second">
-              <li><a href="profile.php#philosophy">企業理念</a></li>
-              <li><a href="profile.php#service">事業紹介</a></li>
-              <li><a href="profile.php#overview">会社概要</a></li>
-              <li><a href="profile.php#access">アクセス</a></li>
-            </ul>
-          </li>
-          <li><a href="gallery.php">商品一覧</a></li>
-          <li><a href="achievements.php">実績紹介</a></li>
-          <li><a href="faq.php"><?php echo FAQ; ?></a></li>
-          <li class="menu-contact"><a href="contact.php">お問い合わせ</a></li>
-        </ul>
-      </nav>
-      
-      <div class="mask"></div>
-    </header>
+    <?php require_once __DIR__ . '/../includes/header.php'; ?>
 
     <main>
       <div class="page-header">
