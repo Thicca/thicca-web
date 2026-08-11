1954ddc30c2e0dcc27b76e14716603293fad3210
thicca <thicca.land@gmail.com>
2026-08-11 16:00:33 +0900

    nav.php調整

--- includes/nav.php
+++ includes/nav.php
@@ -21,10 +21,10 @@
                 <li><a href="profile.php#certificate"><?php echo CERTIFICATE; ?></a></li>
             </ul>
         </li>
-        <li><a href="gallery.php">商品一覧</a></li>
-        <li><a href="achievements.php">実績紹介</a></li>
+        <li><a href="gallery.php"><?php echo GALLERY; ?></a></li>
+        <li><a href="achievements.php"><?php echo ACHIEVEMENTS; ?></a></li>
         <li><a href="faq.php"><?php echo FAQ; ?></a></li>
-        <li class="menu-contact"><a href="contact.php">お問い合わせ</a></li>
+        <li class="menu-contact"><a href="contact.php"><?php echo CONTACT; ?></a></li>
     </ul>
 </nav>
 
