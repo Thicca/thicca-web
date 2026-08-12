b0f652fcc849aeaf12fc9c28cc8eda890dbe4b40
thicca <thicca.land@gmail.com>
2026-08-12 12:35:26 +0900

    profileのpagelink-listにhistory追加

--- public/profile.php
+++ public/profile.php
@@ -28,6 +28,7 @@ $cssFile = 'profile';
         <li><a href="#works"><?php echo WORKS; ?></a></li>
         <li><a href="#ability"><?php echo ABILITY; ?></a></li>
         <li><a href="#certificate"><?php echo CERTIFICATE; ?></a></li>
+        <li><a href="#history"><?php echo HISTORY; ?></a></li>
       </ul>
 
       <section id="aboutme">
