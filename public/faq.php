5bffe6ae561794f30fbbabdf22104e5c45529efc
thicca <thicca.land@gmail.com>
2026-08-12 15:48:41 +0900

    faq.json追加とfaq.php調整

--- public/faq.php
+++ public/faq.php
@@ -4,10 +4,14 @@ $pageTitle = FAQ;
 $pageUrl = SITE_URL . '/';
 $pageDescription = SITE_DESCRIPTION;
 $cssFile = 'faq';
+
+// データ読み込み
+$allItems = json_decode(file_get_contents(__DIR__ . '/../data/faq.json'), true);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
   <?php require_once __DIR__ . '/../includes/head.php'; ?>
+  <script src="js/gallery.js"></script>
   <body>
     <?php require_once __DIR__ . '/../includes/header.php'; ?>
     <main>
@@ -26,58 +30,18 @@ $cssFile = 'faq';
       <p class="head-text"><?php echo FAQ_DESCRIPTION; ?></p>
 
       <dl class="faq-list">
+        <?php foreach ($allItems as $item): ?>
         <div class="item">
           <dt>
             <span class="question">Q</span>
-            <span>インテリアの相談をしたいのですが</span>
+            <span><?php echo htmlspecialchars($item['question']); ?></span>
           </dt>
           <dd>
             <span class="answer">A</span>
-            <span>インテリアのことならなんでもご相談ください。<br>
-              お電話または<a href="contact">お問い合わせフォーム</a>にてご相談を承っております。</span>
+            <span><?php echo $item['answer']; ?></span>
           </dd>
         </div>
-        <div class="item">
-          <dt>
-            <span class="question">Q</span>
-            <span>見積もりは無料ですか？</span>
-          </dt>
-          <dd>
-            <span class="answer">A</span>
-            <span>はい、無料です。ご予算に合わせたご提案も可能ですのでまずはお気軽にご相談ください。</span>
-          </dd>
-        </div>
-        <div class="item">
-          <dt>
-            <span class="question">Q</span>
-            <span>どんな業種の店舗でも対応していますか？</span>
-          </dt>
-          <dd>
-            <span class="answer">A</span>
-            <span>はい、どんな業種の店舗にも対応しています。特にカフェやアパレルは多数の実績があり得意としています。</span>
-          </dd>
-        </div>
-        <div class="item">
-          <dt>
-            <span class="question">Q</span>
-            <span>物件探しからお願いできますか？</span>
-          </dt>
-          <dd>
-            <span class="answer">A</span>
-            <span>はい、社内に物件探しの専門部署がありますので、お客様のご要望に合った物件をお探しいたします。</span>
-          </dd>
-        </div>
-        <div class="item">
-          <dt>
-            <span class="question">Q</span>
-            <span>アフターフォローはどうなっていますか？</span>
-          </dt>
-          <dd>
-            <span class="answer">A</span>
-            <span>24時間体制のアフターフォローサービスを設けています。<br>
-              お困りごとがございましたらお電話やメールにてお気軽にご相談下さい。</span>
-          </dd>
-        </div>
+        <?php endforeach; ?>
       </dl>
     </main>
 
