689dc0958eaf88f193bf48b7aa0cc4939b4df46f
thicca <thicca.land@gmail.com>
2026-08-13 17:29:50 +0900

    タブ画像変更

--- includes/head.php
+++ includes/head.php
@@ -27,8 +27,8 @@ $pageCss = $page . '.css';
 
     <!-- 表示系設定 -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="images/common/favicon.ico">
-    <link rel="apple-touch-icon" href="images/common/apple-touch-icon.png">
+    <link rel="icon" href="images/common/thicca_emote9.ico">
+    <link rel="apple-touch-icon" href="images/common/thicca_emote9.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;500;700&family=Zen+Old+Mincho:wght@400&display=swap" rel="stylesheet">
